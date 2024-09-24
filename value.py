866eed2e22f3f379bb92f797d1462d78fbc7ed8d
hanhvn <vonhathanh@gmail.com>
2024-09-24 18:59:07 +0700

    let me cook

---
+++ value.py
@@ -0,0 +1,25 @@
+class Value:
+    def __init__(self, data, chilren=(), _op='', label=''):
+        self.data = data
+        self._prev = chilren
+        self._op = _op
+        self.label = label
+        self.grad = 0
+
+    def __repr__(self):
+        return f"Value(data={self.data}))"
+
+    def __add__(self, other):
+        return Value(self.data + other.data, (self, other), '+')
+
+    def __mul__(self, other):
+        return Value(self.data * other.data, (self, other), '*')
+
+
+if __name__ == '__main__':
+    a = Value(2.0, label='a')
+    b = Value(-3.0, label='b')
+    c = Value(10.0, label='c')
+
+    d = a*b + c
+    print(d, d._prev, d._op)
