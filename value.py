24d03f5d03650537bbd069ead2dd0beff57a11df
hanhvn <vonhathanh@gmail.com>
2024-09-25 19:03:59 +0700

    let Karpathy cook

--- value.py
+++ value.py
@@ -21,32 +21,55 @@ class Value:
         return f"Value(data={self.data}))"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backdward():
-            self.grad = 1.0 * out.grad
-            other.grad = 1.0 * out.grad
-
-            for c in out._prev:
-                c._backward()
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
 
         out._backward = _backdward
 
         return out
 
+    def __radd__(self, other):
+        return self + other
+
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backdward():
-            self.grad = other.data * out.grad
-            other.grad = self.data * out.grad
-
-            for c in out._prev:
-                c._backward()
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
 
         out._backward = _backdward
 
         return out
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __rtruediv__(self, other):
+        return self * other**-1
+
+    def __pow__(self, power):
+        assert isinstance(power, (int, float), "only supporting integer/float powers for now")
+
+        out = Value(self.data ** power, (self, ), f'**{power}')
+
+        def _backdward():
+            self.grad += power * (self.data ** (power - 1)) * out.grad
+
+        out._backward = _backdward
+
+        return out
+
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
 
     def tanh(self):
         x = self.data
@@ -54,17 +77,44 @@ class Value:
         out = Value(t, (self,), 'tanh')
 
         def _backdward():
-            self.grad = (1 - t**2) * out.grad
-
-            for c in out._prev:
-                c._backward()
+            self.grad += (1 - t**2) * out.grad
 
         out._backward = _backdward
 
         return out
 
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), chilren=(self, ), _op='exp')
+
+        def _backdward():
+            self.grad += out.data * out.grad
+
+        out._backward = _backdward
+
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+
 
 if __name__ == '__main__':
+    a = Value(1.0)
+    print(a+10, a*10)
     x1 = Value(2.0, label='x1')
     x2 = Value(0.0, label='x2')
 
@@ -89,7 +139,6 @@ if __name__ == '__main__':
 
     o.grad = 1.0
 
-    o._backward()
-    # n._backward()
+    o.backward()
 
     draw_dot(o)
